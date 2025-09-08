a7b91a77a1c2d90b59e48b94ac6a9a6b1a2ed3e6
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-09-08 05:52:29 +0000

    updates

---
+++ com/cc/java/ArrayBasic.java
@@ -0,0 +1,41 @@
+package com.cc.java;
+
+public class ArrayBasic
+{
+
+    public static void main(String[] args) {
+       
+        // Deklaration: int
+		// int [] intArr = new int [4];
+		// System.out.println("Länge Array: " + intArr.length);	
+
+        // int [] intArr = {2,8,5,9};
+        // System.out.println("Länge Array: " + intArr.length);	
+        // System.out.println(intArr); // ?
+
+        // Index --> Zeiger auf Elemente
+        // System.out.println(intArr[0]);
+        // System.out.println(intArr[1]);
+        // System.out.println(intArr[2]);
+        // System.out.println(intArr[3]);
+
+
+        // Schleife
+        // for (int i = 0; i < 4; i++) {
+        //     System.out.println("Index: " + i);
+        // }
+
+
+        // Verbinden Schleifenindex --> Arrayindex
+        // for (int i = 0; i < intArr.length; i++) {
+        //     System.out.println("Index: " + i);
+        //     System.out.println(intArr[i]);
+        // }
+
+
+
+    }
+    
+}
+
+
